93779d67ac032a585e6374e6fc636363bd61c272
SilentSys <andy@flankers.net>
2015-09-13 14:47:23 -0700

    v1.0

---
+++ SLAM/Form1.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GameSelector = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ImportButton = New System.Windows.Forms.Button()
+        Me.TrackList = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.StartButton = New System.Windows.Forms.Button()
+        Me.ImportDialog = New System.Windows.Forms.OpenFileDialog()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.WavWorker = New System.ComponentModel.BackgroundWorker()
+        Me.PollRelayWorker = New System.ComponentModel.BackgroundWorker()
+        Me.ChangeDirButton = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PlayKeyTextBox = New System.Windows.Forms.TextBox()
+        Me.ChangeDirDialog = New System.Windows.Forms.FolderBrowserDialog()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.SuspendLayout()
+        '
+        'GameSelector
+        '
+        Me.GameSelector.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GameSelector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.GameSelector.FormattingEnabled = True
+        Me.GameSelector.Location = New System.Drawing.Point(56, 12)
+        Me.GameSelector.MaxDropDownItems = 100
+        Me.GameSelector.Name = "GameSelector"
+        Me.GameSelector.Size = New System.Drawing.Size(335, 21)
+        Me.GameSelector.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Game:"
+        '
+        'ImportButton
+        '
+        Me.ImportButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.ImportButton.Location = New System.Drawing.Point(15, 288)
+        Me.ImportButton.Name = "ImportButton"
+        Me.ImportButton.Size = New System.Drawing.Size(75, 23)
+        Me.ImportButton.TabIndex = 3
+        Me.ImportButton.Text = "Import"
+        Me.ImportButton.UseVisualStyleBackColor = True
+        '
+        'TrackList
+        '
+        Me.TrackList.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TrackList.AutoArrange = False
+        Me.TrackList.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3})
+        Me.TrackList.FullRowSelect = True
+        Me.TrackList.HideSelection = False
+        Me.TrackList.ImeMode = System.Windows.Forms.ImeMode.Off
+        Me.TrackList.Location = New System.Drawing.Point(15, 39)
+        Me.TrackList.MultiSelect = False
+        Me.TrackList.Name = "TrackList"
+        Me.TrackList.Size = New System.Drawing.Size(457, 243)
+        Me.TrackList.TabIndex = 4
+        Me.TrackList.UseCompatibleStateImageBehavior = False
+        Me.TrackList.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Loaded"
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Track"
+        Me.ColumnHeader2.Width = 137
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Tags"
+        Me.ColumnHeader3.Width = 43
+        '
+        'StartButton
+        '
+        Me.StartButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.StartButton.Location = New System.Drawing.Point(96, 288)
+        Me.StartButton.Name = "StartButton"
+        Me.StartButton.Size = New System.Drawing.Size(75, 23)
+        Me.StartButton.TabIndex = 5
+        Me.StartButton.Text = "Start"
+        Me.StartButton.UseVisualStyleBackColor = True
+        '
+        'ImportDialog
+        '
+        Me.ImportDialog.FileName = "ImportDialog"
+        Me.ImportDialog.Filter = "Audio Files|*.mp3;*.wav"
+        Me.ImportDialog.Multiselect = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ProgressBar1.Location = New System.Drawing.Point(15, 317)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(457, 23)
+        Me.ProgressBar1.Step = 1
+        Me.ProgressBar1.TabIndex = 6
+        '
+        'WavWorker
+        '
+        Me.WavWorker.WorkerReportsProgress = True
+        '
+        'PollRelayWorker
+        '
+        Me.PollRelayWorker.WorkerReportsProgress = True
+        Me.PollRelayWorker.WorkerSupportsCancellation = True
+        '
+        'ChangeDirButton
+        '
+        Me.ChangeDirButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ChangeDirButton.Location = New System.Drawing.Point(397, 10)
+        Me.ChangeDirButton.Name = "ChangeDirButton"
+        Me.ChangeDirButton.Size = New System.Drawing.Size(75, 23)
+        Me.ChangeDirButton.TabIndex = 7
+        Me.ChangeDirButton.Text = "Change Dir"
+        Me.ChangeDirButton.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(390, 293)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Play Key:"
+        '
+        'PlayKeyTextBox
+        '
+        Me.PlayKeyTextBox.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PlayKeyTextBox.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.PlayKeyTextBox.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.PlayKeyTextBox.Location = New System.Drawing.Point(447, 290)
+        Me.PlayKeyTextBox.MaxLength = 2
+        Me.PlayKeyTextBox.Name = "PlayKeyTextBox"
+        Me.PlayKeyTextBox.Size = New System.Drawing.Size(25, 20)
+        Me.PlayKeyTextBox.TabIndex = 9
+        Me.PlayKeyTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'ChangeDirDialog
+        '
+        Me.ChangeDirDialog.Description = "Select your steamapps folder:"
+        Me.ChangeDirDialog.ShowNewFolderButton = False
+        '
+        'BackgroundWorker1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(484, 352)
+        Me.Controls.Add(Me.PlayKeyTextBox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ChangeDirButton)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.StartButton)
+        Me.Controls.Add(Me.TrackList)
+        Me.Controls.Add(Me.ImportButton)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GameSelector)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Source Live Audio Mixer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GameSelector As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ImportButton As System.Windows.Forms.Button
+    Friend WithEvents TrackList As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents StartButton As System.Windows.Forms.Button
+    Friend WithEvents ImportDialog As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents WavWorker As System.ComponentModel.BackgroundWorker
+    Friend WithEvents PollRelayWorker As System.ComponentModel.BackgroundWorker
+    Friend WithEvents ChangeDirButton As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents PlayKeyTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents ChangeDirDialog As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+
+End Class
---
+++ SLAM/Form1.vb
@@ -0,0 +1,437 @@
+﻿Imports NAudio 'Modified Version which does not write "extraSize"
+Imports NAudio.Wave
+Imports System.IO
+Imports System.Text.RegularExpressions
+Imports System.Threading
+
+Public Class Form1
+    Dim Games As New List(Of SourceGame)
+
+    Dim SteamappsPath As String = My.Settings.SteamAppsFolder
+    Public PlayKey As String = My.Settings.PlayKey
+    Dim running As Boolean = False
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DisableInterface()
+
+        PlayKeyTextBox.Text = PlayKey
+
+        Dim csgo As New SourceGame
+        csgo.name = "Counter-Strike: Global Offensive"
+        csgo.directory = "common\Counter-Strike Global Offensive\"
+        csgo.ToCfg = "csgo\cfg\"
+        csgo.libraryname = "csgo\"
+        csgo.samplerate = 22050
+        Games.Add(csgo)
+
+        Dim tf2 As New SourceGame
+        tf2.name = "Team Fortress 2"
+        tf2.directory = "common\Team Fortress 2\"
+        tf2.ToCfg = "tf\cfg\"
+        tf2.libraryname = "tf2\"
+        Games.Add(tf2)
+
+        If String.IsNullOrEmpty(SteamappsPath) Then
+            If ChangeDirDialog.ShowDialog = System.Windows.Forms.DialogResult.OK Then
+                SteamappsPath = ChangeDirDialog.SelectedPath & "\"
+            Else
+                Me.Close()
+            End If
+        End If
+
+        LoadGames()
+    End Sub
+
+    Public Shared Sub WaveCreator(File As String, outputFile As String)
+        Dim reader
+
+        If Path.GetExtension(File) = ".mp3" Then
+            reader = New Mp3FileReader(File)
+        ElseIf Path.GetExtension(File) = ".wav" Then
+            reader = New WaveFileReader(File)
+        End If
+
+        Dim outFormat = New WaveFormat(22050, 16, 1)
+
+        Dim resampler = New MediaFoundationResampler(reader, outFormat)
+
+        resampler.ResamplerQuality = 60
+
+        WaveFileWriter.CreateWaveFile(outputFile, resampler)
+
+        resampler.Dispose()
+    End Sub
+
+    Private Sub GameSelector_SelectedIndexChanged(sender As Object, e As EventArgs) Handles GameSelector.SelectedIndexChanged
+        ReloadTracks(GetCurrentGame)
+        RefreshTrackList()
+        My.Settings.LastGame = GameSelector.Text
+        My.Settings.Save()
+    End Sub
+
+    Private Sub ImportButton_Click(sender As Object, e As EventArgs) Handles ImportButton.Click
+        If ImportDialog.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            ProgressBar1.Maximum = ImportDialog.FileNames.Count
+
+            Dim WorkerPassthrough() As Object = {GetCurrentGame(), ImportDialog.FileNames}
+
+            WavWorker.RunWorkerAsync(WorkerPassthrough)
+            DisableInterface()
+        End If
+    End Sub
+
+    Private Sub WavWorker_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles WavWorker.DoWork
+        Dim Game As SourceGame = e.Argument(0)
+        Dim Files() As String = e.Argument(1)
+        Dim FailedFiles As New List(Of String)
+
+        For Each File In Files
+
+            Try
+                Dim OutFile As String = Path.Combine(Game.libraryname, Path.GetFileNameWithoutExtension(File) & ".wav")
+
+                WaveCreator(File, OutFile)
+
+            Catch ex As Exception
+                FailedFiles.Add(File)
+            End Try
+            WavWorker.ReportProgress(0)
+        Next
+
+        e.Result = FailedFiles
+
+    End Sub
+
+    Private Sub WavWorker_ProgressChanged(sender As Object, e As System.ComponentModel.ProgressChangedEventArgs) Handles WavWorker.ProgressChanged
+        ProgressBar1.PerformStep()
+        ReloadTracks(GetCurrentGame)
+        RefreshTrackList()
+    End Sub
+
+    Private Sub WavWorker_RunWorkerCompleted(sender As Object, e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles WavWorker.RunWorkerCompleted
+        ProgressBar1.Value = 0
+        Dim MsgBoxText As String = "Conversion complete!"
+        Dim FailedFiles As New List(Of String)
+
+        For Each FilePath In e.Result
+            FailedFiles.Add(Path.GetFileName(FilePath))
+        Next
+
+        If FailedFiles.Count > 0 Then
+            MsgBoxText = MsgBoxText & " However, the following files failed to convert: " & String.Join(", ", FailedFiles)
+        End If
+
+        ReloadTracks(GetCurrentGame)
+        RefreshTrackList()
+        MsgBox(MsgBoxText)
+        EnableInterface()
+    End Sub
+
+    Private Function GetCurrentGame() As SourceGame
+        For Each Game In Games
+            If Game.name = GameSelector.SelectedItem.ToString Then
+                Return Game
+            End If
+        Next
+    End Function
+
+    Private Sub ReloadTracks(Game As SourceGame)
+        If Directory.Exists(Game.libraryname) Then
+
+            Game.tracks.Clear()
+            For Each File In Directory.GetFiles(Game.libraryname)
+
+                If Game.FileExtension = Path.GetExtension(File) Then
+                    Dim track As New SourceGame.track
+                    track.name = Path.GetFileNameWithoutExtension(File)
+                    Game.tracks.Add(track)
+                End If
+
+            Next
+
+            CreateTags(Game)
+
+        Else
+            Directory.CreateDirectory(Game.libraryname)
+        End If
+    End Sub
+
+    Private Sub RefreshTrackList()
+        TrackList.Items.Clear()
+
+        Dim Game As SourceGame = GetCurrentGame()
+
+        For Each Track In Game.tracks
+            TrackList.Items.Add(New ListViewItem({"False", Track.name, """" & String.Join(""", """, Track.tags) & """"}))
+        Next
+
+
+        TrackList.AutoResizeColumn(0, ColumnHeaderAutoResizeStyle.HeaderSize)
+        TrackList.AutoResizeColumn(1, ColumnHeaderAutoResizeStyle.ColumnContent)
+        TrackList.AutoResizeColumn(2, ColumnHeaderAutoResizeStyle.ColumnContent)
+    End Sub
+
+    Private Sub StartButton_Click(sender As Object, e As EventArgs) Handles StartButton.Click
+        If running Then
+            StopPoll()
+        Else
+            StartPoll()
+            MsgBox("Don't forget to type ""exec slam"" in colsole!")
+        End If
+    End Sub
+
+    Private Sub StartPoll()
+        running = True
+        StartButton.Text = "Stop"
+        CreateCfgFiles()
+        DisableInterface()
+        StartButton.Enabled = True
+        PollRelayWorker.RunWorkerAsync(GetCurrentGame)
+    End Sub
+
+    Private Sub StopPoll()
+        running = False
+        StartButton.Text = "Start"
+        EnableInterface()
+        PollRelayWorker.CancelAsync()
+    End Sub
+
+    Private Sub CreateCfgFiles()
+        Dim Game As SourceGame = GetCurrentGame()
+        Dim GameDir As String = Path.Combine(SteamappsPath, Game.directory)
+        Dim GameCfgFolder As String = Path.Combine(GameDir, Game.ToCfg)
+
+        Using slam_cfg As StreamWriter = New StreamWriter(GameCfgFolder & "slam.cfg")
+            slam_cfg.WriteLine("alias slam_listtracks ""exec slam_tracklist.cfg""")
+            slam_cfg.WriteLine("alias list slam_listtracks")
+            slam_cfg.WriteLine("alias tracks slam_listtracks")
+            slam_cfg.WriteLine("alias la slam_listtracks")
+            slam_cfg.WriteLine("alias slam_play slam_play_on")
+            slam_cfg.WriteLine("alias slam_play_on ""alias slam_play slam_play_off; voice_inputfromfile 1; voice_loopback 1; +voicerecord""")
+            slam_cfg.WriteLine("alias slam_play_off ""-voicerecord; voice_inputfromfile 0; voice_loopback 0; alias slam_play slam_play_on""")
+            slam_cfg.WriteLine("alias slam_updatecfg ""host_writeconfig slam_relay""")
+            slam_cfg.WriteLine("bind {0} slam_play", PlayKey)
+            For Each Track In Game.tracks
+                Dim index As String = Game.tracks.IndexOf(Track)
+                slam_cfg.WriteLine("alias {0} ""bind {1} {0}; slam_updatecfg; echo Loaded: {2}""", index + 1, Game.RelayKey, Track.name)
+
+                For Each TrackTag In Track.tags
+                    slam_cfg.WriteLine("alias {0} ""bind {1} {2}; slam_updatecfg; echo Loaded: {3}""", TrackTag, Game.RelayKey, index + 1, Track.name)
+                Next
+            Next
+            slam_cfg.WriteLine("voice_enable 1; voice_modenable 1; voice_forcemicrecord 0; voice_fadeouttime 0.0; con_enable 1")
+        End Using
+
+        Using slam_tracklist_cfg As StreamWriter = New StreamWriter(GameCfgFolder & "slam_tracklist.cfg")
+            slam_tracklist_cfg.WriteLine("echo ""You can select tracks either by typing a tag, or their track number.""")
+            slam_tracklist_cfg.WriteLine("echo ""--------------------Tracks--------------------""")
+            For Each Track In Game.tracks
+                Dim index As String = Game.tracks.IndexOf(Track)
+                slam_tracklist_cfg.WriteLine("echo ""{0}. {1} [{2}]""", index + 1, Track.name, "'" & String.Join("', '", Track.tags) & "'")
+            Next
+            slam_tracklist_cfg.WriteLine("echo ""----------------------------------------------""")
+        End Using
+
+    End Sub
+
+    Private Function LoadTrack(ByVal Game As SourceGame, ByVal index As Integer) As Boolean
+        Dim TrackName As String
+        If Game.tracks.Count > index Then
+            TrackName = Game.tracks(index).name
+            Dim voicefile As String = Path.Combine(SteamappsPath, Game.directory) & "voice_input.wav"
+
+            Try
+                If File.Exists(voicefile) Then
+                    File.Delete(voicefile)
+                End If
+                File.Copy(Game.libraryname & TrackName & ".wav", voicefile)
+
+            Catch ex As Exception
+                Return False
+            End Try
+
+        End If
+        Return True
+    End Function
+
+    Public Shared Function recog(ByVal str As String, ByVal reg As String) As String
+        Dim keyd As Match = Regex.Match(str, reg)
+        Return (keyd.Groups(1).ToString)
+    End Function
+
+    Private Sub PollRelayWorker_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles PollRelayWorker.DoWork
+        Dim Game As SourceGame = e.Argument
+        Dim GameDir As String = Path.Combine(SteamappsPath, Game.directory)
+        Dim GameCfg As String = Path.Combine(GameDir, Game.ToCfg) & "slam_relay.cfg"
+
+        Do
+            If PollRelayWorker.CancellationPending Then
+                Exit Do
+            End If
+
+            If File.Exists(GameCfg) Then
+                Dim RelayCfg As String
+                Using reader As StreamReader = New StreamReader(GameCfg)
+                    RelayCfg = reader.ReadToEnd
+                End Using
+
+                Dim command As String = recog(RelayCfg, String.Format("bind ""{0}"" ""(.*?)""", Game.RelayKey))
+                If Not String.IsNullOrEmpty(command) Then
+                    'load audiofile
+                    If IsNumeric(command) Then
+                        If LoadTrack(Game, Convert.ToInt32(command) - 1) Then
+                            PollRelayWorker.ReportProgress(Convert.ToInt32(command) - 1)
+                        End If
+                    End If
+                    File.Delete(GameCfg)
+                End If
+            End If
+
+            Thread.Sleep(Game.PollInterval)
+        Loop
+    End Sub
+
+    Private Sub PollRelayWorker_ProgressChanged(sender As Object, e As System.ComponentModel.ProgressChangedEventArgs) Handles PollRelayWorker.ProgressChanged
+        DisplayLoaded(e.ProgressPercentage)
+    End Sub
+
+    Private Sub CreateTags(ByVal Game As SourceGame)
+        Dim NameWords As New Dictionary(Of String, Integer)
+
+        Dim index As Integer
+        For Each Track In Game.tracks
+            Dim Words As List(Of String) = Track.name.Split({" "c, "."c, "-"c, "_"c}).ToList
+
+            For Each Word In Words
+
+                If Not IsNumeric(Word) Then
+                    If NameWords.ContainsKey(Word) Then
+                        NameWords.Remove(Word)
+                    Else
+                        NameWords.Add(Word, index)
+                    End If
+                End If
+
+            Next
+            index += 1
+        Next
+
+        For Each Tag As KeyValuePair(Of String, Integer) In NameWords
+            Game.tracks(Tag.Value).tags.Add(Tag.Key)
+        Next
+    End Sub
+
+    Public Sub EnableInterface()
+        GameSelector.Enabled = True
+        TrackList.Enabled = True
+        ImportButton.Enabled = True
+        StartButton.Enabled = True
+        ChangeDirButton.Enabled = True
+        PlayKeyTextBox.Enabled = True
+    End Sub
+
+    Public Sub DisableInterface()
+        GameSelector.Enabled = False
+        TrackList.Enabled = False
+        ImportButton.Enabled = False
+        StartButton.Enabled = False
+        ChangeDirButton.Enabled = False
+        PlayKeyTextBox.Enabled = False
+        PlayKeyTextBox.Text = PlayKey
+    End Sub
+
+    Public Sub DisplayLoaded(ByVal track As Integer)
+        For i As Integer = 0 To TrackList.Items.Count - 1
+            TrackList.Items(i).SubItems(0).Text = "False"
+        Next
+        TrackList.Items(track).SubItems(0).Text = "True"
+    End Sub
+
+    Private Sub PlayKeyTextBox_Enter(sender As Object, e As EventArgs) Handles PlayKeyTextBox.Enter
+        BeginInvoke(DirectCast(Sub() PlayKeyTextBox.SelectAll(), Action))
+    End Sub
+
+    Private Sub PlayKey_TextChanged(sender As Object, e As EventArgs) Handles PlayKeyTextBox.TextChanged
+        If String.IsNullOrEmpty(PlayKeyTextBox.Text) Then
+            PlayKeyTextBox.Text = PlayKey
+        End If
+
+        If PlayKeyTextBox.Text.Count > 1 Then
+            PlayKeyTextBox.Text = PlayKeyTextBox.Text.Substring(1)
+        End If
+
+        PlayKeyTextBox.SelectAll()
+        PlayKey = PlayKeyTextBox.Text
+
+        My.Settings.PlayKey = PlayKey
+        My.Settings.Save()
+    End Sub
+
+    Private Sub ChangeDirButton_Click(sender As Object, e As EventArgs) Handles ChangeDirButton.Click
+        If ChangeDirDialog.ShowDialog = System.Windows.Forms.DialogResult.OK Then
+            SteamappsPath = ChangeDirDialog.SelectedPath & "\"
+            LoadGames()
+        End If
+    End Sub
+
+    Public Sub LoadGames()
+        GameSelector.Items.Clear()
+
+        For Each Game In Games
+            Dim GameFullPath As String = Path.Combine(SteamappsPath, Game.directory)
+
+            If Directory.Exists(GameFullPath) Then
+                GameSelector.Items.Add(Game.name)
+            End If
+
+        Next
+
+
+        If GameSelector.Items.Count > 0 Then
+            Dim GoToIndex As String = 0
+
+            If GameSelector.Items.Contains(My.Settings.LastGame) Then
+                GoToIndex = GameSelector.Items.IndexOf(My.Settings.LastGame)
+            End If
+
+            GameSelector.Text = GameSelector.Items(GoToIndex).ToString
+            ReloadTracks(GetCurrentGame)
+            RefreshTrackList()
+
+            My.Settings.SteamAppsFolder = SteamappsPath
+            My.Settings.Save()
+
+            EnableInterface()
+
+        Else
+            MsgBox("There are no games in your steamapps filder. Please click ""Change Dir"" and select a valid steamapps folder.")
+            DisableInterface()
+            ChangeDirButton.Enabled = True
+        End If
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+
+    End Sub
+End Class
+
+Public Class SourceGame
+    Public name As String
+    Public directory As String
+    Public ToCfg As String
+    Public libraryname As String
+
+    Public FileExtension As String = ".wav"
+    Public samplerate As Integer = 11025
+    Public bits As Integer = 16
+    Public channels As Integer = 1
+
+    Public PollInterval As Integer = 100
+    Public RelayKey As String = "="
+
+    Public tracks As New List(Of track)
+    Public Class track
+        Public name As String
+        Public tags As New List(Of String)
+    End Class
+End Class
