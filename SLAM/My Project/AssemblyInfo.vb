976e10d1dc0eb2775ebe7faeee05e8fb9067dd6f
SilentSys <andy@flankers.net>
2017-01-20 21:04:17 -0800

    v1.4.0

--- SLAM/My Project/AssemblyInfo.vb
+++ SLAM/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("193a12a3-ca78-4aeb-ab44-ec7f53364f7d")> 
+<Assembly: Guid("193a12a3-ca78-4aeb-ab44-ec7f53364f7d")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.3.4.0")> 
-<Assembly: AssemblyFileVersion("1.3.4.0")> 
+<Assembly: AssemblyVersion("1.4.0.0")>
+<Assembly: AssemblyFileVersion("1.4.0.0")>
