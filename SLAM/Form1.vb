52c6c07d9142f66283cf65e30608a3523be63757
SilentSys <andy@flankers.net>
2017-08-10 09:29:20 -0700

    Fixed tracks not playing after being trimmed or having their volume changed

--- SLAM/Form1.vb
+++ SLAM/Form1.vb
@@ -167,10 +167,10 @@ Public Class Form1
 
         Dim trimstring As String
         If length > 0 Then
-            trimstring = String.Format("-ss {0} -t {1} ", starttrim, length)
+            trimstring = String.Format("-ss {0} -t {1} ", starttrim.ToString("F5", Globalization.CultureInfo.InvariantCulture), length.ToString("F5", Globalization.CultureInfo.InvariantCulture))
         End If
 
-        Dim command As String = String.Format("-i ""{0}"" -f wav -flags bitexact -map_metadata -1 -vn -acodec pcm_s16le -ar {1} -ac {2} {3}-af ""volume={4}"" ""{5}""", Path.GetFullPath(inpath), samplerate, channels, trimstring, volume, Path.GetFullPath(outpath))
+        Dim command As String = String.Format("-i ""{0}"" -f wav -flags bitexact -map_metadata -1 -vn -acodec pcm_s16le -ar {1} -ac {2} {3}-af ""volume={4}"" ""{5}""", Path.GetFullPath(inpath), samplerate, channels, trimstring, volume.ToString("F5", Globalization.CultureInfo.InvariantCulture), Path.GetFullPath(outpath))
         convert.Invoke(command)
     End Sub
 
--- SLAM/My Project/AssemblyInfo.vb
+++ SLAM/My Project/AssemblyInfo.vb
@@ -11,8 +11,8 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyTitle("SLAM")> 
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("SLAM")> 
-<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyProduct("SLAM")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
